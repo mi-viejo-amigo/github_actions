8467be6a20fd571407c5e3b8ecf856d83cb0f9d5
FROL <nkt.frlv7@yandex.ru>
2024-10-06 10:36:16 +0200

    added new button google analitics check

--- src/components/myBox/MyBox.tsx
+++ src/components/myBox/MyBox.tsx
@@ -2,12 +2,23 @@ import styles from './myBox.module.scss';
 import useMetrika from '../../hooks/useMetrika';
 
 const MyBox = (): JSX.Element => {
-	const { ym } = useMetrika();
+	const { ym, gtag } = useMetrika();
 
 	return (
-		<button className={styles.card} onClick={() => ym('reachGoal', 'buy')}>
-			<span className={styles.title}>В Корзину</span>
-		</button>
+		<>
+			<button className={styles.card} onClick={() => ym('reachGoal', 'buy')}>
+				<span className={styles.title}>В Корзину (Yandex-Metrika)</span>
+			</button>
+			<button
+				className={styles.card}
+				onClick={() =>
+					gtag('event', 'add_to_basket', {
+						event_name: 'add_to_basket',
+					})
+				}>
+				<span className={styles.title}>В Корзину (Google-Analitics)</span>
+			</button>
+		</>
 	);
 };
 
